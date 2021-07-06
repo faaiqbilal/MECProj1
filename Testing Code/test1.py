7a18103b77b7225e922fefe74acd77e210d74fd5
faaiqbilal <faaiqbilal@outlook.com>
2021-07-06 19:57:16 +0500

    update

--- Testing Code/test1.py
+++ Testing Code/test1.py
@@ -1,7 +1,23 @@
 from __future__ import print_function
 import time
 import swagger_client
+from swagger_client.configuration import Configuration
 from swagger_client.rest import ApiException
 from pprint import pprint
 
-api_instance = swagger_client.RniApi(swagger_client.ApiClient())
+
+configuration = Configuration()
+configuration.host = "https://try-mec.etsi.org/sbxyljdrf7/rni/v2"
+
+# create an instance of the API class
+api_instance = swagger_client.RniApi(swagger_client.ApiClient(configuration))
+app_ins_id = ['10.10.0.1'] # list[str] | Comma separated list of Application instance identifiers
+
+try:
+    # Retrieve information on the underlying Mobile Network that the MEC application is associated to
+    api_response = api_instance.plmn_info_get(app_ins_id)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling RniApi->plmn_info_get: %s\n" % e)
+
+
