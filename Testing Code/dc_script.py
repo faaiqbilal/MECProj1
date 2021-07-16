f94dd282280417b2cc695063f64303b0f5ad406c
faaiqbilal <faaiqbilal@outlook.com>
2021-07-17 04:44:41 +0500

    some changes

--- Testing Code/dc_script.py
+++ Testing Code/dc_script.py
@@ -2,22 +2,14 @@ from __future__ import print_function
 import time
 import swagger_client
 from swagger_client.configuration import Configuration
-import json
 from swagger_client.rest import ApiException
 from pprint import pprint
 
-
 configuration = Configuration()
-configuration.host = "https://try-mec.etsi.org/sbxq2fy6x5/rni/v2"
+configuration.host = "https://try-mec.etsi.org/sbxa33vybk/rni/v2"
 
-# create an instance of the API class
 api_instance = swagger_client.RniApi(swagger_client.ApiClient(configuration))
+
 app_ins_id = ['10.10.0.1', '10.100.0.1'] # list[str] | Comma separated list of Application instance identifiers
 
-try:
-    api_response = api_instance.rab_info_get()
-    # pprint(api_response)
-    loaded = json.load(api_response)
-    print(type(loaded))
-except ApiException as e:
-    print("Exception when calling RniApi->rab_info_get: %s\n" % e)
+
--- Testing Code/keep_server_active.py
+++ Testing Code/keep_server_active.py
@@ -2,9 +2,10 @@ from __future__ import print_function
 import time
 import swagger_client
 from swagger_client.configuration import Configuration
-import json
 from swagger_client.rest import ApiException
+import json
 from pprint import pprint
+from time import sleep
 
 
 configuration = Configuration()
@@ -12,12 +13,14 @@ configuration.host = "https://try-mec.etsi.org/sbxq2fy6x5/rni/v2"
 
 # create an instance of the API class
 api_instance = swagger_client.RniApi(swagger_client.ApiClient(configuration))
-app_ins_id = ['10.10.0.1', '10.100.0.1'] # list[str] | Comma separated list of Application instance identifiers
 
-try:
-    api_response = api_instance.rab_info_get()
-    # pprint(api_response)
-    loaded = json.load(api_response)
-    print(type(loaded))
-except ApiException as e:
-    print("Exception when calling RniApi->rab_info_get: %s\n" % e)
+app_ins_id = ['10.10.0.1']
+
+while(True):    
+    try:
+        # Retrieve information on the underlying Mobile Network that the MEC application is associated to
+        api_response = api_instance.plmn_info_get(app_ins_id)
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when calling RniApi->plmn_info_get: %s\n" % e)
+    sleep(60)
